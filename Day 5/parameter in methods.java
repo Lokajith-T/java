3d11f5132cf2152ea6af7f26ce2eb194e0602a77
Lokajith-T <lokajith4@gmail.com>
2026-03-16 16:27:30 +0530

    Create parameter in methods.java

---
+++ Day 5/parameter in methods.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    static void add(int a, int b) {
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+
+    public static void main(String[] args) {
+        add(5, 3);
+    }
+}
