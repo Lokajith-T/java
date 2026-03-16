20ff37e02777bed2ef9c17ce9560e32a26e5b317
Lokajith-T <lokajith4@gmail.com>
2026-03-16 16:28:04 +0530

    Create Instant method.java

---
+++ Day 5/Instant method.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    void display() {
+        System.out.println("Instance Method");
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.display();
+    }
+}
